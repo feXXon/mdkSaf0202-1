bb302f7dcb62718042ed6a87966d49afb7bfda3e
Robert Guzaerov <Robertguzaerov123@yandex.ru>
2024-09-04 23:06:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int egg = 4;
+        if (egg > 3)
+            System.out.println("Рекомендую приготовить омлет");
+        else
+            System.out.println("Рекомендую позавтракать бутербродами");
+
+        System.out.print("Приятного аппетита!");
+    }
+}
